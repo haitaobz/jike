1aef52658ddd245b1af6641abf05d01264668558
haitaobz <27667290@qq.com>
2020-05-04 13:00:36 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <form name="form1" method="post" action="">
-<label><input type="checkbox" name="checkbox[]" value="复选一"></label>
-<label><input type="checkbox" name="checkbox[]" value="复选二"></label>复选二
-<label><input type="checkbox" name="checkbox[]" value="复选三"></label>复选三
-<label><input type="checkbox" name="checkbox[]" value="复选四"></label>复选四
+<label><input type="checkbox" name="r1[]" value="复选一"></label>
+<label><input type="checkbox" name="r1[]" value="复选二"></label>复选二
+<label><input type="checkbox" name="r1[]" value="复选三"></label>复选三
+<label><input type="checkbox" name="r1[]" value="复选四"></label>复选四
 
  
 
@@ -11,7 +11,7 @@
 <?php
 if( $_POST )
 {
-$value = $_POST['checkbox'];
+$value = $_POST['r1'];
 echo '你的选择:'.implode(',',$value);
 }
 ?>
