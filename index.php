ddf1488e7ce63b0ea5213c3ebd3921233110d296
haitaobz <27667290@qq.com>
2020-05-04 14:17:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,15 +17,56 @@
   <br>
   
   <dt>贵族等级:</dt> 
-  <dd><label>VIP <input class="ipt txt txt1" type="text" name="r3" id="r3"><span>请输入数字</span></label></dd> 
-  <br>   
-
+  <dd><label>VIP <input class="ipt txt txt1" type="text" name="r3"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>具体大区:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r4"><span>例：128区</span></label></dd> 
+  <br>  
+  
+  <dt>英雄数量:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r5"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>皮肤数量:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r6"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>五级铭文数量:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r7"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>荣耀典藏数量:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r8"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>王者印记:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r9"><span>请输入数字</span></label></dd> 
+  <br>
+  
+  <dt>当前段位:</dt> 
+  <dd><label> <input class="ipt txt txt1" type="text" name="r10"><span>例：钻石4</span></label></dd> 
+  <br>
+  
+  
   <dt>限定英雄:</dt> 
-  <dd><label> <input name="r4[]" type="checkbox" value="韩信">韩信</label></span></dd>
-  <dd><label> <input name="r4[]" type="checkbox" value="娜可露露">娜可露露</label></span></dd>
-  <dd><label> <input name="r4[]" type="checkbox" value="橘右京">橘右京</label></span></dd>
-  <dd><label> <input name="r4[]" type="checkbox" value="不知火舞">不知火舞 </label></span></dd>
+  <dd><label> <input name="r11[]" type="checkbox" value="韩信">韩信</label></dd>
+  <dd><label> <input name="r11[]" type="checkbox" value="娜可露露">娜可露露</label></dd>
+  <dd><label> <input name="r11[]" type="checkbox" value="橘右京">橘右京</label></dd>
+  <dd><label> <input name="r11[]" type="checkbox" value="不知火舞">不知火舞 </label></dd>
 
+  <dt>荣耀水晶详细:</dt> 
+  <dd><label> <input name="r12[]" type="checkbox" value="活力突击">活力突击</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="无限飓风号">无限飓风号</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="全息碎影">全息碎影</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="武则天">武则天</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="天鹅之梦">天鹅之梦</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="武则天">武则天</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="大秦宣太后">大秦宣太后</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="杀手不太冷">杀手不太冷</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="星空梦想">星空梦想</label></dd>
+  <dd><label> <input name="r12[]" type="checkbox" value="倪克斯神谕">倪克斯神谕</label></dd>
+  
 
 <input type="submit" value="提交">
 </form>
@@ -39,11 +80,14 @@
   
 贵族等级:<?php echo $_POST["r3"]; ?>
 
-限定英雄:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r4']; } 
+限定英雄:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r11']; } 
+         foreach ($area_arr as $k=>$v){ echo $v.","; } ?> 
+  
+荣耀水晶详细:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r12']; } 
          foreach ($area_arr as $k=>$v){ echo $v.","; } ?> 
 </textarea>
 <br> 
-<input type="button" value="点击复制代码" onclick="Copy()">
+<input type="button" value="点击复制给客服" onclick="Copy()">
 
 <script>
     function Copy() {
