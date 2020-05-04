4a08a80e86465cd12d736d5b68f83042a4ca6f3b
haitaobz <27667290@qq.com>
2020-05-04 12:19:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,34 +5,22 @@
 </head>
 <body>
 <form action="?" method="get">
-<dt>
-区服系统选项:</dt> 
-<dd> 
-    <label> <input class="ipt" type="radio" value="苹果QQ" name="r1">
-         苹果QQ</label>
-      </dd> 
-      <dd> 
-       <label> <input class="ipt" type="radio" value="苹果VX" name="r1">苹果微信 </label> 
-      </dd> 
-      <dd> 
-       <label> <input class="ipt" type="radio" value="安卓QQ" name="r1">安卓QQ </label> 
-      </dd> 
-      <dd> 
-       <label> <input class="ipt" type="radio" value="安卓VX" name="r1">安卓微信 </label> 
-      </dd> 
-          
-            <input name="HF_pingtai" type="hidden" id="HF_pingtai">
+<dt>区服系统选项:</dt> 
+<dd><label> <input class="ipt" type="radio" value="苹果QQ" name="r1">苹果QQ</label></dd> 
+<dd><label> <input class="ipt" type="radio" value="苹果VX" name="r1">苹果微信 </label></dd> 
+<dd><label> <input class="ipt" type="radio" value="安卓QQ" name="r1">安卓QQ </label></dd> 
+<dd><label> <input class="ipt" type="radio" value="安卓VX" name="r1">安卓微信 </label></dd> 
 
-      <dt>
-<br>
-账号性别: <input type="text" name="2">
+<dt>账号性别:</dt> 
+<dd><label> <input class="ipt" type="radio" value="男" name="r2">男</label></dd> 
+<dd><label> <input class="ipt" type="radio" value="女" name="r2">女</label></dd>
+<br>   
 <input type="submit" value="提交">
 </form>
 </body>
 </html>
 
 <textarea autoHeight="true">
-区服系统选项<?php echo $_GET["r1"]; ?>
-
+区服系统选项<?php echo $_GET["r1"];\r?>
 账号性别<?php echo $_GET["2"]; ?>
 </textarea>
