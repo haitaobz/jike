8a54db6dfd0de02790e4707cd011ad8ad73af902
haitaobz <27667290@qq.com>
2020-05-04 14:46:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -73,27 +73,97 @@
   <label> <input name="r13[]" type="checkbox" value="如梦令(后羿)">如梦令(后羿)</label>
   <label> <input name="r13[]" type="checkbox" value="如梦令(嫦娥)">如梦令(嫦娥)</label>
   <label> <input name="r13[]" type="checkbox" value="幽恒">幽恒</label>
-  <label> <input name="r13[]" type="checkbox" value="玄嵩">玄嵩</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="时之彼端">时之彼端</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="岱宗">岱宗</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="飞衡">飞衡</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="太华">太华</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="猫影幻舞">猫影幻舞</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="天狼征服者">天狼征服者</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="梁祝">梁祝</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="玩趣恶龙">玩趣恶龙</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="神奇女侠">神奇女侠</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="云霓雀翎">云霓雀翎</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="冰封战神">冰封战神</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="凤求凰">凤求凰</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="天魔缭乱">天魔缭乱</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="至尊宝">至尊宝</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="麒麟志">麒麟志</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="奇迹圣诞">奇迹圣诞</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="朱雀志">朱雀志</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="白虎志">白虎志</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="美猴王">美猴王</label></dd>
-  <dd><label> <input name="r13[]" type="checkbox" value="青龙志">青龙志</label></dd>
+  <label> <input name="r13[]" type="checkbox" value="玄嵩">玄嵩</label>
+  <label> <input name="r13[]" type="checkbox" value="时之彼端">时之彼端</label>
+  <label> <input name="r13[]" type="checkbox" value="岱宗">岱宗</label>
+  <label> <input name="r13[]" type="checkbox" value="飞衡">飞衡</label>
+  <label> <input name="r13[]" type="checkbox" value="太华">太华</label>
+  <label> <input name="r13[]" type="checkbox" value="猫影幻舞">猫影幻舞</label>
+  <label> <input name="r13[]" type="checkbox" value="天狼征服者">天狼征服者</label>
+  <label> <input name="r13[]" type="checkbox" value="梁祝">梁祝</label>
+  <label> <input name="r13[]" type="checkbox" value="玩趣恶龙">玩趣恶龙</label>
+  <label> <input name="r13[]" type="checkbox" value="神奇女侠">神奇女侠</label>
+  <label> <input name="r13[]" type="checkbox" value="云霓雀翎">云霓雀翎</label>
+  <label> <input name="r13[]" type="checkbox" value="冰封战神">冰封战神</label>
+  <label> <input name="r13[]" type="checkbox" value="凤求凰">凤求凰</label>
+  <label> <input name="r13[]" type="checkbox" value="天魔缭乱">天魔缭乱</label>
+  <label> <input name="r13[]" type="checkbox" value="至尊宝">至尊宝</label>
+  <label> <input name="r13[]" type="checkbox" value="麒麟志">麒麟志</label>
+  <label> <input name="r13[]" type="checkbox" value="奇迹圣诞">奇迹圣诞</label>
+  <label> <input name="r13[]" type="checkbox" value="朱雀志">朱雀志</label>
+  <label> <input name="r13[]" type="checkbox" value="白虎志">白虎志</label>
+  <label> <input name="r13[]" type="checkbox" value="美猴王">美猴王</label>
+  <label> <input name="r13[]" type="checkbox" value="青龙志">青龙志</label>
+  <label> <input name="r13[]" type="checkbox" value="霸王别姬（男）">霸王别姬（男）</label>
+  <label> <input name="r13[]" type="checkbox" value="霸王别姬（女）">霸王别姬（女）</label>
+  <label> <input name="r13[]" type="checkbox" value="一生所爱">一生所爱</label>
+  <label> <input name="r13[]" type="checkbox" value="冰霜恋舞曲">冰霜恋舞曲</label>
+  <label> <input name="r13[]" type="checkbox" value="大圣娶亲"> 大圣娶亲</label>
+  <label> <input name="r13[]" type="checkbox" value="逐梦之影">逐梦之影</label>
+  <label> <input name="r13[]" type="checkbox" value="逐梦之翼">逐梦之翼</label>
+  <label> <input name="r13[]" type="checkbox" value="逐梦之光">逐梦之光</label>
+  <label> <input name="r13[]" type="checkbox" value="逐梦之音">逐梦之音</label>
+  <label> <input name="r13[]" type="checkbox" value="逐梦之星">逐梦之星</label>
+  <label> <input name="r13[]" type="checkbox" value="圣诞老人">圣诞老人</label>
+  <label> <input name="r13[]" type="checkbox" value="梅西">梅西</label>
+  <label> <input name="r13[]" type="checkbox" value="蜜橘之夏">蜜橘之夏</label>
+  <label> <input name="r13[]" type="checkbox" value="纯白花嫁">纯白花嫁</label>
+  <label> <input name="r13[]" type="checkbox" value="真爱至上">真爱至上</label>
+  <label> <input name="r13[]" type="checkbox" value="青春决赛季">青春决赛季</label>
+  <label> <input name="r13[]" type="checkbox" value="武陵仙君">武陵仙君</label>
+  <label> <input name="r13[]" type="checkbox" value="热情桑巴">热情桑巴</label>
+  <label> <input name="r13[]" type="checkbox" value="辉光之辰">辉光之辰</label>
+  <label> <input name="r13[]" type="checkbox" value="永耀之星">永耀之星</label>
+  <label> <input name="r13[]" type="checkbox" value="云端筑梦师">云端筑梦师</label>
+  <label> <input name="r13[]" type="checkbox" value="凤凰于飞">凤凰于飞</label>
+  <label> <input name="r13[]" type="checkbox" value="魔法小厨娘">魔法小厨娘</label>
+  <label> <input name="r13[]" type="checkbox" value="引擎之心">引擎之心</label>
+  <label> <input name="r13[]" type="checkbox" value="死亡摇滚">死亡摇滚</label>
+  <label> <input name="r13[]" type="checkbox" value="职业王牌">职业王牌</label>
+  <label> <input name="r13[]" type="checkbox" value="嘻哈天王">嘻哈天王</label>
+  <label> <input name="r13[]" type="checkbox" value="死神来了">死神来了</label>
+  <label> <input name="r13[]" type="checkbox" value="华尔街大亨">华尔街大亨</label>
+  <label> <input name="r13[]" type="checkbox" value="天堂福音">天堂福音</label>
+  <label> <input name="r13[]" type="checkbox" value="仙境爱丽丝">仙境爱丽丝</label>
+  <label> <input name="r13[]" type="checkbox" value="纽约教父">纽约教父</label>
+  <label> <input name="r13[]" type="checkbox" value="幻想奇妙夜">幻想奇妙夜</label>
+  <label> <input name="r13[]" type="checkbox" value="化身博士">化身博士</label>
+  <label> <input name="r13[]" type="checkbox" value="火炮千金">火炮千金</label>
+  <label> <input name="r13[]" type="checkbox" value="黑猫爱糖果">黑猫爱糖果</label>
+  <label> <input name="r13[]" type="checkbox" value="进击墨子号">进击墨子号</label>
+  <label> <input name="r13[]" type="checkbox" value="游园惊梦">游园惊梦</label>
+  <label> <input name="r13[]" type="checkbox" value="王者之锤">王者之锤</label>
+  <label> <input name="r13[]" type="checkbox" value="狮心王">狮心王</label>
+  <label> <input name="r13[]" type="checkbox" value="蜃楼王">蜃楼王</label>
+  <label> <input name="r13[]" type="checkbox" value="妖精王">妖精王</label>
+  <label> <input name="r13[]" type="checkbox" value="炼金王">炼金王</label>
+  <label> <input name="r13[]" type="checkbox" value="凯尔特女王">凯尔特女王</label>
+  <label> <input name="r13[]" type="checkbox" value="狰">狰</label>
+  <label> <input name="r13[]" type="checkbox" value="饕餮">饕餮</label>
+  <label> <input name="r13[]" type="checkbox" value="烛龙">烛龙</label>
+  <label> <input name="r13[]" type="checkbox" value="重明">重明</label>
+  <label> <input name="r13[]" type="checkbox" value="拳王">拳王</label>
+  <label> <input name="r13[]" type="checkbox" value="穷奇">穷奇</label>
+  <label> <input name="r13[]" type="checkbox" value="御狮">御狮</label>
+  <label> <input name="r13[]" type="checkbox" value="遇见飞天">遇见飞天</label>
+  <label> <input name="r13[]" type="checkbox" value="幻乐之宴">幻乐之宴</label>
+  <label> <input name="r13[]" type="checkbox" value="太乙真人">太乙真人</label>
+  <label> <input name="r13[]" type="checkbox" value="猎兽之王">猎兽之王</label>
+  <label> <input name="r13[]" type="checkbox" value="白昼王子">白昼王子</label>
+  <label> <input name="r13[]" type="checkbox" value=" 天狼狩猎者"> 天狼狩猎者</label></dd>
+
+  <dt>史诗皮肤:</dt> 
+  <dd><label> <input name="r14[]" type="checkbox" value="遇见神鹿">遇见神鹿</label>
+  <label> <input name="r14[]" type="checkbox" value="修罗">修罗</label>
+  <label> <input name="r14[]" type="checkbox" value="地狱岩魂">地狱岩魂</label>
+  <label> <input name="r14[]" type="checkbox" value="魅语">魅语</label>
+  <label> <input name="r14[]" type="checkbox" value="青蛇">青蛇</label>
+  <label> <input name="r14[]" type="checkbox" value="白蛇">白蛇</label>
+  <label> <input name="r14[]" type="checkbox" value="晚萤">晚萤</label>
+  <label> <input name="r14[]" type="checkbox" value="箭雨风息">箭雨风息</label>
+  <label> <input name="r14[]" type="checkbox" value="暗隐猎兽者">暗隐猎兽者</label>
+  <label> <input name="r14[]" type="checkbox" value="无限星赏官">无限星赏官</label>
+  <label> <input name="r14[]" type="checkbox" value=" 曙光守护者"> 曙光守护者</label></dd>
 
 <input type="submit" value="提交">
 </form>
@@ -112,6 +182,8 @@
 荣耀水晶详细:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r12']; } 
          foreach ($area_arr as $k=>$v){ echo $v.","; } ?> 
 限定皮肤:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r13']; } 
+         foreach ($area_arr as $k=>$v){ echo $v.","; } ?> 
+史诗皮肤:<?php $area_arr = array(); if($_GET['action']=="submit"){ $area_arr = $_POST['r14']; } 
          foreach ($area_arr as $k=>$v){ echo $v.","; } ?> 
 </textarea>
 <br> 
