b5b8570b1c7a40cdb498e10f4bc7bc1b4621f788
haitaobz <27667290@qq.com>
2020-05-04 18:23:05 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,63 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<head> 
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" /> 
-<title>php获取多选框checkbox值</title> 
-</head> 
-<body> 
-<?php 
-$area_arr = array(); 
-if($_GET['action']=="submit"){ 
-$area_arr = $_POST['area']; 
-} 
-echo "您选定的地区为: "; 
-foreach ($area_arr as $k=>$v){ 
-echo $v." "; 
-} 
-?> 
-区服系统选项:<?php echo $_POST["r1"];?>&#10;
-<form id="form1" name="form1" method="post" action="?action=submit"> 
-<p>河北 
-<label> 
-<input type="checkbox" id="area" name="area[]" value="河北"> 
-</label> 
-</p> 
-<p>河南 
-<label> 
-<input type="checkbox" id="area[]" name="area[]" value="河南"> 
-</label> 
-</p> 
-<p>山西 
-<label> 
-<input type="checkbox" id="area[]" name="area[]" value="山西"> 
-</label> 
-</p> 
-<p>山东 
-<label> 
-<input type="checkbox" id="area[]" name="area[]" value="山东"> 
-</label> 
-</p> 
-<p>江苏 
-<label> 
-<input type="checkbox" id="area[]" name="area[]" value="江苏"> 
-</label> 
-</p> 
-<p>浙江 
-<label> 
-<input type="checkbox" id="area[]" name="area[]" value="浙江"> 
-</label> 
-</p> 
-<p> 
-<label> 
-<dt>区服系统选项:</dt> 
-  <dd><label> <input class="ipt" type="radio" value="苹果QQ" name="r1">苹果QQ</label></dd> 
-  <dd><label> <input class="ipt" type="radio" value="苹果VX" name="r1">苹果微信 </label></dd> 
-  <dd><label> <input class="ipt" type="radio" value="安卓QQ" name="r1">安卓QQ </label></dd> 
-  <dd><label> <input class="ipt" type="radio" value="安卓VX" name="r1">安卓微信 </label></dd> 
-
-<input type="submit" name="Submit" value="提交"> 
-</label> 
-</p> 
-</form> 
-</body> 
-</html> 
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="slide-content">1</div>
+  <div>2</div>
+  </body>
